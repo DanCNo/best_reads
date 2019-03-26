822993eb3463350eb020835d20e527a342684aee
App Academy Student <aa@Apps-Mac-mini.local>
2019-03-26 17:52:51 -0400

    push to heroku again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
 
 	validates :username, :email, :password_digest, :session_token, presence: true
-	validates :username, :email uniqueness: true
+	validates :username, :email, uniqueness: true
 	validates :password, length: {minimum: 6}, allow_nil: true
 
-	attr_reader: password
+	attr_reader :password
 
 	after_initialize :ensure_session_token
 

a70e4c66708891311af5de9293c2c9a7b10e8b75
DanCNo <dan.noh@gmail.com>
2019-04-21 15:11:53 -0400

    add reviews reducer

---
+++ frontend/reducers/reviews_reducer.js
@@ -0,0 +1,33 @@
+import {
+  RECEIVE_REVIEW,
+  RECEIVE_REVIEWS,
+  REMOVE_REVIEW,
+} from '../actions/review_actions';
+import merge from 'lodash/merge';
+
+const reviewsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState;
+
+  switch(action.type){
+
+    case RECEIVE_REVIEWS:
+
+      return action.reviews;
+
+    case RECEIVE_REVIEW:
+
+      return merge( {}, state, {[action.review.id]: action.review});
+
+    case REMOVE_REVIEW:
+
+      newState = merge({}, state);
+      delete newState[action.review.id];
+      return newState;
+
+    default:
+      return state;
+  }
+};
+
+export default reviewsReducer;

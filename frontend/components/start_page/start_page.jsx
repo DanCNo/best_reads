c651dca215bba2b2010e06b56242f60ac3fb1a68
DanCNo <dan.noh@gmail.com>
2019-05-15 14:43:02 -0400

    add footer to start page and add selling points section

--- frontend/components/start_page/start_page.jsx
+++ frontend/components/start_page/start_page.jsx
@@ -24,6 +24,34 @@ const StartPage = () => {
           < SignUpContainer />
         </div>
       </div>
+
+      <div className="selling-point-box">
+        <div className="selling-point">
+          <h2 className="selling-headline">Deciding What to Read Next?</h2>
+          Browse through our selection and read reviews. Keep track of what you have read and have not read.
+        </div>
+        <div className="selling-point">
+          <h2 className="selling-headline">What are other people reading?</h2>
+          What are other people reading and what do they think about your favorite and least favorite books?
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="per-nav-links-container">
+          <div className="per-nav-link">
+            <a href="https://dancno.github.io/"><i className="fas fa-laptop-code"></i></a>
+          </div>
+          <div className="per-nav-link">
+            <a href="https://angel.co/dancno"><i className="fab fa-angellist"></i></a>
+          </div>
+          <div className="per-nav-link">
+            <a href="https://github.com/DanCNo"><i className="fab fa-github"></i></a>
+          </div>
+          <div className="per-nav-link">
+            <a href="https://www.linkedin.com/in/dan-n-480b2856/"><i className="fab fa-linkedin"></i></a>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
